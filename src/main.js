bdba3ef8ffa0281cc41dcab8718daa556169ebb9
Zychaowill <15536542139@163.com>
2017-03-27 17:40:15 +0800

    Initializing package structure.

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@
 import Vue from 'vue';
 import App from './App';
 import router from './router';
+import axios from 'axios';
 import vuex from 'vuex';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.min';
