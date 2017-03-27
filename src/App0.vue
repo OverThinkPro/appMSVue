5040f5c1e1b4731e38a43d3e3d48b4f50954429f
Zychaowill <15536542139@163.com>
2017-03-27 19:00:47 +0800

    commit test.

---
+++ src/App0.vue
@@ -0,0 +1,40 @@
+<template>
+  <div id="app" class="my-container">
+    <v-header class="head"></v-header>
+    <router-view></router-view>
+    <v-footer class="foot"></v-footer>
+  </div>
+</template>
+
+<script>
+import VHeader from './components/Header/Header.vue';
+import VFooter from './components/Footer/Footer.vue';
+
+export default {
+  name: 'app',
+  data () {
+    return {
+
+    }
+  },
+  components: {
+    VHeader, VFooter
+  }
+};
+</script>
+
+<style>
+.my-container {
+  flex: auto;
+  text-align: center;
+}
+
+#app .head {
+
+}
+
+#app .foot {
+  position: fixed;
+  bottom: 0;
+}
+</style>
